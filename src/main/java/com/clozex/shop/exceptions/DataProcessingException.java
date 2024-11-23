7ce418d8bb5f0a54e6ad4320e78de610a09ac1c4
clo3eX <pansydaisy83@gmail.com>
2024-11-23 14:06:43 +0100

    added suggested changes

---
+++ src/main/java/com/clozex/shop/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.clozex.shop.exceptions;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/clozex/shop/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/clozex/shop/repository/impl/BookRepositoryImpl.java
@@ -1,20 +1,20 @@
 package com.clozex.shop.repository.impl;
 
+import com.clozex.shop.exceptions.DataProcessingException;
 import com.clozex.shop.model.Book;
 import com.clozex.shop.repository.BookRepository;
 import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
-    private SessionFactory sessionFactory;
-
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    private final SessionFactory sessionFactory;
 
     @Override
     public Book save(Book book) {
@@ -30,7 +30,7 @@ public class BookRepositoryImpl implements BookRepository {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can`t save book" + book, e);
+            throw new DataProcessingException("Can`t save book" + book);
         } finally {
             if (session != null) {
                 session.close();
@@ -43,6 +43,8 @@ public class BookRepositoryImpl implements BookRepository {
         try (Session session = sessionFactory.openSession()) {
             return session.createQuery("FROM Book ", Book.class)
                     .getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t find all books");
         }
     }
 }
