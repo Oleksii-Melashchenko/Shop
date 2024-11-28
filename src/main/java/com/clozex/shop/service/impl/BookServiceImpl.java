3585a668d6d44b61e700216ccd7d6c5ddd73c755
clo3eX <pansydaisy83@gmail.com>
2024-11-28 13:19:38 +0100

    some improvements

--- src/main/java/com/clozex/shop/service/impl/BookServiceImpl.java
+++ src/main/java/com/clozex/shop/service/impl/BookServiceImpl.java
@@ -9,10 +9,8 @@ import com.clozex.shop.model.Book;
 import com.clozex.shop.repository.book.BookRepository;
 import com.clozex.shop.repository.book.BookSpecificationBuilder;
 import com.clozex.shop.service.BookService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -32,10 +30,8 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Page<BookDto> findAll(Pageable pageable) {
-        List<BookDto> bookDtos = bookRepository.findAll(pageable).stream()
-                .map(bookMapper::toDto)
-                .toList();
-        return new PageImpl<>(bookDtos);
+        return bookRepository.findAll(pageable)
+            .map(bookMapper::toDto);
     }
 
     @Override
@@ -62,9 +58,7 @@ public class BookServiceImpl implements BookService {
     @Override
     public Page<BookDto> searchBooks(BookSearchParametersDto searchParams, Pageable pageable) {
         Specification<Book> specification = bookSpecificationBuilder.build(searchParams);
-        List<BookDto> bookDtos = bookRepository.findAll(specification, pageable).stream()
-                .map(bookMapper::toDto)
-                .toList();
-        return new PageImpl<>(bookDtos);
+        return bookRepository.findAll(specification,pageable)
+                .map(bookMapper::toDto);
     }
 }
